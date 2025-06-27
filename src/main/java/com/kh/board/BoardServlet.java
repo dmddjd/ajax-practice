78a3467df6c5eeb51c1bc9a3e3126ecd1baf9f04
dmddjd <dmstjd99510@naver.com>
2025-06-27 17:48:10 +0900

    나-락락

--- src/main/java/com/kh/board/BoardServlet.java
+++ src/main/java/com/kh/board/BoardServlet.java
@@ -1,16 +1,23 @@
 package com.kh.board;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kh.board.model.vo.Board;
 
 /**
  * Servlet implementation class BoardServlet
  */
-@WebServlet("/BoardServlet")
+@WebServlet("/board")
 public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +33,25 @@ public class BoardServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		List<Board> list = (List<Board>)session.getAttribute("list");
+		
+		if(list == null) {
+			list = new ArrayList<>();
+			list.add(new Board(1,"kh게시판 1번글","admin","안녕하세요1",new Date()));
+			list.add(new Board(2,"kh게시판 2번글","admin","안녕하세요2",new Date()));
+			list.add(new Board(3,"kh게시판 1번글","mkm","안녕하세요3",new Date()));
+			list.add(new Board(4,"kh게시판 4번글","user01","안녕하세요4",new Date()));
+			list.add(new Board(5,"kh게시판 5번글","user02","안녕하세요5",new Date()));
+			
+			session.setAttribute("list", list);
+		}
+		
+		
+		
+		
+		request.getRequestDispatcher("/board/board.jsp").forward(request, response);
+		
 	}
 
 	/**
